a005d626bdbeae29df92ab422d52c76e4f3222b2
leejh82 <leejh3558@gmail.com>
2022-04-08 00:29:27 +0900

    4

--- lib/constant_widget/constant_preview.dart
+++ lib/constant_widget/constant_preview.dart
@@ -4,19 +4,37 @@ import 'package:flutter/material.dart';
 import 'constant.dart';
 
 class ConstantPreview extends StatelessWidget {
-
-  final List colorName = [
-  BackgroundColor,
-  TitleTextColor,
-  BodyTextColor,
-  TextLightColor,
-  InfectedColor,
-  DeathColor,
-  Recovercolor,
-  PrimaryColor,
-  ShadowColor,
-  ActiveShadowColor,
+  static List<String> colorName = [
+    'BackgroundColor',
+    'TitleTextColor',
+    'BodyTextColor',
+    'TextLightColor',
+    'InfectedColor',
+    'DeathColor',
+    'Recovercolor',
+    'PrimaryColor',
+    'ShadowColor',
+    'ActiveShadowColor',
   ];
+  static List<Color> colorCode = [
+    Color(0xFFFEFEFE),
+    Color(0xFF303030),
+    Color(0xFF4B4B4B),
+    Color(0xFF959595),
+    Color(0xFFFF8748),
+    Color(0xFFFF4848),
+    Color(0xFF36C12C),
+    Color(0xFF3382CC),
+    Color(0xFFB7B7B7).withOpacity(.16),
+    Color(0xFF4056C6).withOpacity(.15),
+  ];
+
+  final List<ColorName> colorData = List.generate(
+      colorName.length,
+      (index) => ColorName(
+            colorName[index],
+            colorCode[index],
+          ));
 
   ConstantPreview({Key? key}) : super(key: key);
 
@@ -29,36 +47,50 @@ class ConstantPreview extends StatelessWidget {
       body: ListView.builder(
         itemCount: colorName.length,
         itemBuilder: (context, index) {
-        return Container(
-          margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-          width: double.maxFinite,
-          padding: EdgeInsets.all(20),
-          color: Colors.white,
-          child: Row(
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: colorName[index],
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0,5),
-                        blurRadius: 10,
-                        color: ShadowColor,
-                      )
-                    ]
+          return Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            width: double.maxFinite,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 5),
+                    blurRadius: 10,
+                    color: ShadowColor,
+                  )
+                ]),
+            child: Row(
+              children: [
+                Container(
+                  width: 180,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        bottomLeft: Radius.circular(15)),
+                    color: colorData[index].color,
+                  ),
                 ),
-              ),
-              SizedBox(width: 20,),
-              Text('$colorName', style: TitleTextstyle,)
-            ],
-          ),
-        );
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  colorData[index].name,
+                  style: TitleTextstyle,
+                )
+              ],
+            ),
+          );
         },
       ),
-
     );
   }
+}
+
+class ColorName {
+  final String name;
+  final Color color;
+
+  ColorName(this.name, this.color);
 }
